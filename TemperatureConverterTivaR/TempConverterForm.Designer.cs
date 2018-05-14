dc328b7e459aa40306e514c9fbe90175b366daa3
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-05-14 14:31:25 -0400

    Finished

--- TemperatureConverterTivaR/TempConverterForm.Designer.cs
+++ TemperatureConverterTivaR/TempConverterForm.Designer.cs
@@ -28,20 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.txtTemperature = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(64, 63);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(197, 13);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Enter the temperature in degrees Celsius";
+            // 
+            // txtTemperature
+            // 
+            this.txtTemperature.Location = new System.Drawing.Point(299, 63);
+            this.txtTemperature.Name = "txtTemperature";
+            this.txtTemperature.Size = new System.Drawing.Size(100, 20);
+            this.txtTemperature.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(203, 141);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(87, 55);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert to Farenheit";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
             // 
             // frmTempConverter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(527, 208);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtTemperature);
+            this.Controls.Add(this.lblInstructions);
             this.Name = "frmTempConverter";
             this.Text = "Temperature Converter By Tiva Rait";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox txtTemperature;
+        private System.Windows.Forms.Button btnConvert;
     }
 }
 
--- TemperatureConverterTivaR/TempConverterForm.cs
+++ TemperatureConverterTivaR/TempConverterForm.cs
@@ -7,6 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 14-05-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Pass by Value
+ * This program converts a temperature in celsius to the equivalent in fahrenheit
+*/
 
 namespace TemperatureConverterTivaR
 {
@@ -15,6 +22,33 @@ namespace TemperatureConverterTivaR
         public frmTempConverter()
         {
             InitializeComponent();
+        }
+
+        // Function ConvertTemperature 
+        // Input: 
+        // Output: void
+        // Description: This function converts celsius to fahrenheit
+        public void ConvertTemperature (double celsius)
+        {
+            // Declare local variables
+            double fahrenheit;
+
+            fahrenheit = ((double)9/(double)5 * celsius + 32);
+
+            // Display message box with answer
+            MessageBox.Show(celsius + " degrees Celsius = " + fahrenheit + " degrees Fahrenheit", "Celsius to Fahrenheit Converter");
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // Declare local variables
+            double usercelsius;
+
+            // Get the users temperature from the text box
+            usercelsius = Convert.ToDouble(this.txtTemperature.Text);
+
+            // Call the function
+            this.ConvertTemperature(usercelsius);
         }
     }
 }
